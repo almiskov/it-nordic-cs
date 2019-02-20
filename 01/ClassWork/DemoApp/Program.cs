af74f115dadd3e8d0a30a6e33a2ab744f2f7e70a
Alexander Miskov <almiskov@gmail.com>
2019-02-20 21:47:30 +0300

    Lesson01ClassworkAdded

---
+++ 01/ClassWork/DemoApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите имя:");
+			string name = Console.ReadLine();
+			Thread.Sleep(1000);
+			Console.WriteLine($"Приветствую, {name}!");
+			Console.ReadKey();
+		}
+	}
+}
