a00f1e11fd789c28651e526ecdf5ec42d46aed0a
Alexander Miskov <almiskov@gmail.com>
2019-03-30 13:34:30 +0300

    Lesson 11 homework added

---
+++ 11/Homework/Reminder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Reminder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ReminderItem reminder1 = new ReminderItem(
+				DateTimeOffset.Parse("2019-04-05 07:00:00"),
+				"Первый будильник");
+
+			ReminderItem reminder2 = new ReminderItem(
+				DateTimeOffset.Now + TimeSpan.FromSeconds(1),
+				"Второй будильник");
+
+			Thread.Sleep(2000);
+
+			reminder1.WriteProperties();
+			reminder2.WriteProperties();
+
+			Console.ReadKey();
+		}
+	}
+}
